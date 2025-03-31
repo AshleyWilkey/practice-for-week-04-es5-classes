11a8f971b546dfda41b2ce03cc8953775b5b5ed7
Ashley Wilkey <contact@ashleywilkey.com>
2025-03-31 11:30:44 -0700

    refactor: move classes to es5

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,8 @@
-// Your code here
+Array.prototype.isEqual = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== this[i]) {
+      return false;
+    }
+  }
+  return true;
+};
